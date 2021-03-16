e1a720dda321a3c494e3c5b16d678ec55cb7272e
Luis Arteaga <arteaga@live.de>
2021-03-16 05:40:35 +0100

    improve api_auth_service

--- etl_service/api_auth_service/api_keys.py
+++ etl_service/api_auth_service/api_keys.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
+import os
+
+AUTH_DIRECTORY = './etl_service/api_auth_service/.auth'
+
+
+class APIKeys:
+    def __init__(self):
+        self.folder = {'openweathermap.org': 'openweathermap.txt'}
+
 
 def get_api_key(provider):
-    if provider == 'openweathermap.org':
-        with open('./api_auth_service/.auth/openweathermap.txt') as f:
-            api_key = f.read()
+    api_keys = APIKeys()
+    with open(os.path.join(AUTH_DIRECTORY, api_keys.folder[provider])) as f:
+        api_key = f.read()
 
         return api_key
