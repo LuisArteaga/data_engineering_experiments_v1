97b2d31b88cf8824c327abb42f236050c99a50f9
Luis Arteaga <arteaga@live.de>
2021-03-16 05:00:10 +0100

    create general schema check for json data and include openweathermap specific schema check

--- data_extraction_service/api_extraction.py
+++ data_extraction_service/api_extraction.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 import api_auth_service.api_keys as ak
-import data_validation_service.openweathermap_schema as owms
+import data_validation_service.check_schema as owms
 import requests
+
 
 def get_current_weather_data(provider, city):
     if provider == 'openweathermap.org':
         api_key = ak.get_api_key(provider)
-        r = requests.get(url=f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}')
-        raw_data = r.json()
-        raw_data_validated = validate_data(raw_data=raw_data, provider=provider)
+        raw_data = requests.get(url=f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}')
+        raw_data_validated = owms.validate_json_data(raw_data=raw_data, provider=provider)
+
+        return raw_data_validated
